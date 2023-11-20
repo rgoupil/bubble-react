2a3c1957597086bdbaa08831798288671a232020
Robin Goupil <goupil.robin@gmail.com>
2023-11-20 16:11:59 +0200

    handle multiple components

---
+++ dist/components/MathPlaceholder/bubble.d.ts
@@ -0,0 +1,5 @@
+declare const _default: () => {
+    initPlugin: (instance: import("../../shared/BubbleInstance").BubbleInstance<any>, context: import("../../shared/BubbleContext").BubbleContext) => import("../../shared").StateManager<any>;
+    previewPlugin: (instance: import("../../shared/BubbleInstance").BubbleInstance<any>, properties: any) => void;
+} | undefined;
+export default _default;
---
+++ dist/components/MathPlaceholder/index.d.ts
@@ -0,0 +1,5 @@
+export interface Props {
+    a: number;
+    b: number;
+}
+export declare function MathPlaceholder({ a, b }: Props): import("react/jsx-runtime").JSX.Element;
